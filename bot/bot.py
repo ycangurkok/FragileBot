6f91fe57f223f3e48de0500468b4d8e10365d07e
ycangurkok <ycangurkok@gmail.com>
2025-01-20 00:14:22 +0300

    Fixed behavior, improved error handling.

--- bot/bot.py
+++ bot/bot.py
@@ -2,17 +2,32 @@ import discord
 from discord.ext import commands
 from music import Music
 from help import Help
+import os
 
-allIntents = discord.Intents.all()
-bot = commands.Bot(command_prefix="-", intents=allIntents)
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="-", intents=intents)
 bot.remove_command("help")
 
 @bot.event
 async def on_ready():
-
     await bot.add_cog(Music(bot))
     await bot.add_cog(Help(bot))
     await bot.change_presence(activity=discord.Activity(name='music', type=2))
     print(f"Logged in as {bot.user}")
 
-bot.run("ODgyMjAyMDg5NDUzOTQwODA3.GJMc6j.I5_XkEqZ8AB8S3OxhPgVAyxo09Ur7YcTGWpJXU")
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Command not found.")
+    else:
+        await ctx.send("An error occurred.")
+    print(f"An error occurred: {error}")
+
+if TOKEN:
+    bot.run(TOKEN)
+else:
+    print("Error: DISCORD_BOT_TOKEN environment variable not set.")
